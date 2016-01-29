19b90b88f358aa715d52518d4fc68607aba47d4e
wildelf <psizzlepip@yahoo.com>
2016-01-28 19:54:19 -0800

    milestone 1 complete (week2)

--- InstagramEE/PhotosViewController.swift
+++ InstagramEE/PhotosViewController.swift
@@ -10,73 +10,94 @@ import UIKit
 import AFNetworking
 
 class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-
-    @IBOutlet weak var tableView: UITableView!
+	
+	
+	@IBOutlet weak var tableView: UITableView!
+	
 	var grams: [NSDictionary]?
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-            tableView.rowHeight = 320
-            tableView.dataSource = self
-            tableView.delegate = self
-        
-			let clientId = "e05c462ebd86446ea48a5af73769b602"
-			let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
-			let request = NSURLRequest(URL: url!)
-			let session = NSURLSession(
-				configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-				delegate:nil,
-				delegateQueue:NSOperationQueue.mainQueue()
-			)
-			
-			let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-				completionHandler: { (dataOrNil, response, error) in
-					if let data = dataOrNil {
-      if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-				data, options:[]) as? NSDictionary {
-					//NSLog("response: \(responseDictionary)")
-					self.grams = responseDictionary["data"] as? [NSDictionary]
-                    self.tableView.reloadData()
-                        }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		tableView.rowHeight = 320
+		tableView.dataSource = self
+		tableView.delegate = self
+		
+		let clientId = "e05c462ebd86446ea48a5af73769b602"
+		let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+		let request = NSURLRequest(URL: url!)
+		let session = NSURLSession(
+			configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+			delegate:nil,
+			delegateQueue:NSOperationQueue.mainQueue()
+		)
+		
+		let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+			completionHandler: { (dataOrNil, response, error) in
+				if let data = dataOrNil {
+					if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+						data, options:[]) as? NSDictionary {
+							//NSLog("response: \(responseDictionary)")
+							self.grams = responseDictionary["data"] as? [NSDictionary]
+							self.tableView.reloadData()
 					}
-			});
-			task.resume()
+				}
+		});
+		task.resume()
+		
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("gramCell", forIndexPath: indexPath) as! GramTableViewCell
+		
+		let gram = grams![indexPath.row]
+		let profileURL = gram.valueForKeyPath("images.standard_resolution.url") as! String
+		
+		let imageUrl = NSURL(string: profileURL)
+		NSLog("URL: \(profileURL)")
+		cell.gramView2.setImageWithURL(imageUrl!)
+		cell.gramView2.sizeToFit()
+		
+		return cell
+	}
 
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("gramCell", forIndexPath: indexPath) as! GramTableViewCell
-        
-        let gram = grams![indexPath.row]
-        let profileURL = gram.valueForKeyPath("images.standard_resolution.url") as! String
-        
-        let imageUrl = NSURL(string: profileURL)
-        NSLog("URL: \(profileURL)")
-        cell.gramView2.setImageWithURL(imageUrl!)
-        cell.gramView2.sizeToFit()
-        
-      return cell
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let grams = grams {
-            return grams.count
-        } else {
-            return 0
-        }
-    }
-    /*
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        // do something here
-    }
-*/
+	func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+		tableView.deselectRowAtIndexPath(indexPath, animated: true)
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if let grams = grams {
+			return grams.count
+		} else {
+			return 0
+		}
+	}
+	/*
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+	tableView.deselectRowAtIndexPath(indexPath, animated: true)
+	// do something here
+	}
+	
+	*/
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		
+		let vc = segue.destinationViewController as! PhotoDetailsViewController
+		
+		let indexPath = tableView.indexPathForCell(sender as! UITableViewCell)
+		
+		let gram = grams![indexPath!.row]
+		let profileURL = gram.valueForKeyPath("images.standard_resolution.url") as! String
+		
+		let imageUrl = NSURL(string: profileURL)
+		
+		vc.photoURL = imageUrl!
+		
+	}
 }
 
